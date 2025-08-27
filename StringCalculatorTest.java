8a21f75b6134d364f5e03ac767815ecc7519e781
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-08-27 21:39:35 +0530

    Create StringCalculatorTest.java

---
+++ StringCalculatorTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    private final StringCalculator calculator = new StringCalculator();
+
+    @Test
+    void testEmptyString() {
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    void testSingleNumber() {
+        assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    void testTwoNumbers() {
+        assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    void testMultipleNumbers() {
+        assertEquals(10, calculator.add("1,2,3,4"));
+    }
+
+    @Test
+    void testNewlineDelimiter() {
+        assertEquals(6, calculator.add("1\n2,3"));
+    }
+
+    @Test
+    void testBadFormatting() {
+        assertThrows(NumberFormatException.class, () -> calculator.add("1,\n2"));
+    }
+
+    @Test
+    void testCustomDelimiter() {
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+
+}
