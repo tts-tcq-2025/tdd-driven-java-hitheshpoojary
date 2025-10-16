081883f618ef8565b5ce1156aa9d8d023dcbbea5
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-10-16 16:00:08 +0530

    Update StringCalculatorTest.java

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,5 +1,8 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+package stringCalculator
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringCalculatorTest {
 
